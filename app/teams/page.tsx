241ba41cbc59fddf71e8330b88af405eee888753
sik1j <karkisikij2@gmail.com>
2023-12-19 12:03:47 -0800

    Add Teams page component

---
+++ app/teams/page.tsx
@@ -0,0 +1,9 @@
+import Teams from "@/app/ui/teams";
+
+export default async function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Teams />
+   </main>
+  )
+}
---
+++ app/ui/teams.tsx
@@ -0,0 +1,23 @@
+import { getAllTeams } from "@/app/lib/actions";
+import Link from "next/link";
+import { Team } from "../lib/definitions";
+
+export default async function Teams() {
+  const teams = await getAllTeams();
+  function getTeamIdentifier(team: Team) {
+    return team.link.split("=")[1];
+  }
+
+  return (
+    <div>
+      <h1>Teams</h1>
+      <ul>
+        {teams.map((team) => (
+          <li key={team.id}>
+            <Link href={`/teams/${getTeamIdentifier(team)}`}>{team.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
