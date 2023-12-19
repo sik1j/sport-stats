6d5d942b1751ef9263ba96daa36258741e7118ef
sik1j <karkisikij2@gmail.com>
2023-12-19 12:06:32 -0800

    Add page to display players on team

---
+++ app/teams/[id]/page.tsx
@@ -0,0 +1,28 @@
+import { getAllPlayerLinksFromTeam, getPlayerName } from "@/app/lib/actions";
+import Link from "next/link";
+
+export default async function Page({params} : {params: {id: string}}) {
+    function getPlayerESPNid(playerLink: string) {
+        return playerLink.split("/")[7];
+    }
+
+    const id = params.id;
+    const playerLinks = await getAllPlayerLinksFromTeam(`https://www.espn.com/nba/teams/roster?team=${id}`);
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>Players</h1>
+            <ul>
+                {playerLinks.map(async (playerLink) => {
+                    const {firstName, lastName} = await getPlayerName(playerLink);
+                    const id = getPlayerESPNid(playerLink);
+                    return (
+                    <li key={playerLink}>
+                        <Link href={`/players/${id}`}>{`${firstName} ${lastName}`}</Link>
+                    </li>
+                ) 
+                })}
+            </ul>
+        </main>
+    )
+}
