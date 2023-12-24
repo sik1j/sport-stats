1dde9f7e9812efa4f2669c0ee4b0d5f504e32484
sik1j <karkisikij2@gmail.com>
2023-12-24 13:22:06 -0800

    seed.js: Add player seeding functionality

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,5 +1,70 @@
 const { db } = require("@vercel/postgres");
-const { teamLinks } = require("./teamLinks.js");
+const {
+  getAllTeams,
+  getAllPlayerLinksFromTeam,
+  getPlayerName,
+  getEspnIdFromLink,
+  getAllTeamObjects
+} = require("./actions.js");
+
+async function seedPlayers(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    // create table `players` if it doesn't exist
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS players (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        team_id UUID REFERENCES teams(id),
+        espn_id INT NOT NULL
+      );
+    `;
+
+    console.log("Created table `players`");
+
+    const allTeams = await getAllTeams();
+
+    const insertAllPlayers = await Promise.all(
+      allTeams.map(async (team) => {
+        console.log(`Inserting players for ${team.name}`);
+        const playerLinks = await getAllPlayerLinksFromTeam(team.link);
+
+        const insertPlayers = await Promise.all(
+          playerLinks.map(async (playerLink) => {
+            const { firstName, lastName } = await getPlayerName(playerLink);
+            const espnId = getEspnIdFromLink(playerLink);
+
+            return client.sql`
+            INSERT INTO players (name, team_id, espn_id)
+            VALUES (${firstName + " " + lastName}, ${team.id}, ${espnId})
+          `;
+          })
+        );
+
+        console.log(`Inserted ${insertPlayers.length} players for ${team.name}`);
+        return insertPlayers;
+      }
+      )
+    );
+
+    // both playersInserted and allPlayers are counts of players inserted
+    const playersInserted = insertAllPlayers.reduce(
+      (acc, curr) => acc + curr.length,
+      0
+    );
+    const allPlayers = insertAllPlayers.flat();
+
+    console.log(`Inserted ${playersInserted} | ${allPlayers.length} players into table`);
+
+    return {
+      createTable,
+      allPlayers
+    };
+  } catch (error) {
+    console.error("Error seeding players table:", error);
+    throw error;
+  }
+}
 
 async function seedTeams(client) {
   try {
@@ -23,7 +88,7 @@ async function seedTeams(client) {
         (team) =>
           client.sql`
           INSERT INTO teams (name, link)
-          VALUES (${team.name}, ${'https://www.espn.com' + team.link})
+          VALUES (${team.name}, ${'https://www.espn.com' + team.relativeLink})
         `
       )
     );
@@ -43,7 +108,7 @@ async function seedTeams(client) {
 async function main() {
   const client = await db.connect();
 
-  await seedTeams(client);
+  // await seedTeams(client);
 
   // await seedPlayers(client);
 
