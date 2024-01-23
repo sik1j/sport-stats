c383647ad63537b76cef262fd3bb68311c6d4c6f
sik1j <karkisikij2@gmail.com>
2024-01-22 21:10:22 -0800

    Update next.config.js to use ESM syntax

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    /* config options here */
+  }
+   
+  export default nextConfig
